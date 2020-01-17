f49258ca0455ea8d43ff7970cdce3e535e7afee7
Kim Chemnitz Chemnitz <hatelag1@gmail.com>
2020-01-17 11:58:52 +0100

    Added console with add sub div, mul

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        private const double V = 0.0;
+
+        static void Main(string[] args)
+        {
+            double a = V;
+            double c = V;
+            Console.Write("A = ");
+            a = Convert.ToInt32( Console.ReadLine() );
+            Console.Write("B = ");
+            double b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Function = ");
+            string v = Console.ReadLine();
+            string f = v;
+            switch (f)
+            {
+                case "+":
+                    c = a + b;
+                    break;
+                case "-":
+                    c = a - b;
+                    break;
+                case "*":
+                    c = a * b;
+                    break;
+                case "/":
+                    c = a / b;
+                    break;
+            }
+            Console.WriteLine( $"C = {c}");
+            Console.ReadKey();
+        }
+    }
+}
